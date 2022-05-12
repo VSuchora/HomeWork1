fc4133dd437417ac1557978cdc4291dc530b4809
VSuchora <Vladimir.suchora@gmail.com>
2022-05-12 19:33:42 +0300

    HomeWork 1

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let a = 2
+    let b = 9
+    let c = 6
+    let d = 8
+    
+    let q = 0.5
+    let w = 0.7
+    let e = 0.9
+    let r = 0.2
+    
+    var num = 25
+    var double = 2.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(<#T##Any#>)
-        // Do any additional setup after loading the view.
+        print(a+b+c+d)
+        print(q+w+e+r)
+        
+        num = 25
+        switch num {
+        case num/2:
+            print("Even number")
+        default:
+            print("Odd number")
+        }
+        
+        double = 2.3
+        switch num {
+        case num/2:
+            print("Even number")
+        default:
+            print("Odd number")
+        }
     }
-
-
 }
 
