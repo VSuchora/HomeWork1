13830a5766296481bfd97cfc8730443766db67f0
VSuchora <Vladimir.suchora@gmail.com>
2022-05-14 21:41:48 +0300

    sdad
    asas

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -9,40 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let a = 2
-    let b = 9
-    let c = 6
-    let d = 8
+    var a = 2.5, b = 9.7, c = 6.9, d = 8.2
     
-    let q = 0.5
-    let w = 0.7
-    let e = 0.9
-    let r = 0.2
+    var q = 2.5, w = 9.7, e = 6.9, r = 8.2
     
-    var num = 25
-    var double = 2.3
+    let num = 25
+    let num1 = 2.299999999999999
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        a = Double(Int(a)); b = Double(Int(b)); c = Double(Int(c)); d = Double(Int(d))
+        
+        q = q - Double(Int(q)); w = w - Double(Int(w)); e = e - Double(Int(e)); r = r - Double(Int(r))
+        
         print(a+b+c+d)
+        
         print(q+w+e+r)
         
-        num = 25
         switch num {
-        case num/2:
+        case _ where num % 2 == 0:
             print("Even number")
         default:
             print("Odd number")
         }
         
-        double = 2.3
-        switch num {
-        case num/2:
+        if num1.truncatingRemainder(dividingBy: 2) == 0 {
             print("Even number")
-        default:
+        }
+        else {
             print("Odd number")
         }
     }
+    
 }
-
