99c70ef507057262c05a82f6ea78632bc90b1785
VSuchora <Vladimir.suchora@gmail.com>
2022-05-15 22:03:41 +0300

    HomeWork 2

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -9,37 +9,88 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var a = 2.5, b = 9.7, c = 6.9, d = 8.2
     
-    var q = 2.5, w = 9.7, e = 6.9, r = 8.2
-    
-    let num = 25
-    let num1 = 2.299999999999999
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        a = Double(Int(a)); b = Double(Int(b)); c = Double(Int(c)); d = Double(Int(d))
-        
-        q = q - Double(Int(q)); w = w - Double(Int(w)); e = e - Double(Int(e)); r = r - Double(Int(r))
-        
-        print(a+b+c+d)
-        
-        print(q+w+e+r)
-        
-        switch num {
-        case _ where num % 2 == 0:
-            print("Even number")
-        default:
-            print("Odd number")
-        }
-        
-        if num1.truncatingRemainder(dividingBy: 2) == 0 {
-            print("Even number")
-        }
-        else {
-            print("Odd number")
+        addition()
+        addition1()
+        subtraction(170, 33, 47)
+        subtraction(150.85, 38.35, 44.16)
+        multiply()
+        multiply1()
+        divide(80, 4, 2)
+        divide(1923.65, 14.15, 2.2)
+        pervy.abv("авб", "ввш")
+        vtoroi.stepen(6)
+        treti.factorial(7)
+        chetyri.sumnum(1234)
+    }
+    func addition(a: Int = 26, b: Int = 33, c: Int = 92) {
+        print(a+b+c)
+    }
+    func addition1(a: Double = 2.6, b: Double = 3.3, c: Double = 9.2) {
+        print(a+b+c)
+    }
+    func subtraction(_ a: Int,_ b: Int,_ c: Int) {
+        print(a-b-c)
+    }
+    func subtraction(_ a: Double,_ b: Double,_ c: Double) {
+        print(a-b-c)
+    }
+    func multiply(a: Int = 26, b: Int = 33, c: Int = 92) {
+        print(a*b*c)
+    }
+    func multiply1(a: Double = 2.6, b: Double = 3.3, c: Double = 9.2) {
+        print(a*b*c)
+    }
+    func divide(_ a: Int,_ b: Int,_ c: Int) {
+        print(a/b/c)
+    }
+    func divide(_ a: Double,_ b: Double,_ c: Double) {
+        print(a/b/c)
+    }
+    
+    class pervy{
+        static func abv(_ a: String,_ b: String){
+            if a > b{
+                print("\(a) больше \(b)")
+            }
+            else if a < b{
+                print("\(a) меньше \(b)")
+            }
+            else {
+                print("\(a) равен \(b)")
+            }
         }
     }
-    // что происходит с GIT'ом?
+    class vtoroi{
+        static func stepen(_ a: Int, b: Double = 2) {
+            let intpow = Int(pow(Double(a), b))
+            let intb = Int(b)
+            print("\(a) ^ \(intb) = \(intpow)")
+        }
+    }
+    // Задание со степенью не понял. Мб мозг устал. Поэтому копирнул. И понятно и непонятно. Может потом сам разберусь, а может и нет. Но Если объяснят на языке камней и листьев, то будет замечательно...
+    class treti{
+        static func factorial( _ a: Int) {
+            var fact = 1
+            for n in 1...a {
+                fact = fact * n
+            }
+            print(fact)
+        }
+    }
+    // Тоже не особо вдохновился. Под вечер вообще мозги плывут. Но тут что-то понятнее. Но по факту в общем не понял
+    class chetyri{
+        static func sumnum(_ a: Int) {
+            var sum = 0
+            let str = String(a)
+            for n in str {
+                sum += Int(String(n)) ?? 0
+                print("Summ = \(sum)")
+            }
+        }
+    }
+    // Второе скопировал у человека - у которого всё якобы правильно. Не понимаю, почему не выда>т одно итоговое значение. Тяжко с этим. Надо дорабоать
 }
